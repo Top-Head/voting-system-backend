7e074958adb227c11de3ac5cb0e923c6b5903b19
omariscode <omarscode007@gmail.com>
2025-04-26 08:49:47 +0100

    Adicionei uma view para o voter que armazena o google na db, nova url e também configurei o cors

--- api/urls.py
+++ api/urls.py
@@ -5,12 +5,10 @@ urlpatterns = [
 # ---------------------------------------------Admin-----------------------------------------------
     path('api/admin/login', views.admin_login, name='admin_token'),
     path('api/admin/create-category', views.create_category, name='create_category'),
-    path('api/admin/update-category/<int:id>', views.update_category, name='update-category'),
-    path('api/admin/close-category/<int:id>', views.close_category, name='close-category'),
-    path('api/admin/create-project', views.create_project, name='create-project'),
-    path('api/admin/update-project/<int:id>', views.update_project, name='update-project')
-
-
-
+    path('api/admin/update-category/<int:id>', views.update_category, name='update_category'),
+    path('api/admin/close-category/<int:id>', views.close_category, name='close_category'),
+    path('api/admin/create-project', views.create_project, name='create_project'),
+    path('api/admin/update-project/<int:id>', views.update_project, name='update_project'),
 #---------------------------------------------Voter-------------------------------------------------
+    path('api/login', views.voter_login, name='voter_login'),
 ]
