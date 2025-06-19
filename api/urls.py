c1d1f6f87414d2abcd720c401dbbda569d9d32e8
omariscode <omarscode007@gmail.com>
2025-06-19 21:06:09 +0100

    Atualizei algumas cenas

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,6 @@ from api import views
 urlpatterns = [
 # ---------------------------------------------Admin-----------------------------------------------
     path('category-autocomplete/', views.CategoryAutocomplete.as_view(), name='category-autocomplete'),
-    path('api/admin/create-category', views.create_category, name='create_category'),
-    path('api/admin/update-category/<int:id>', views.update_category, name='update_category'),
     path('api/admin/close-activity/<int:id>', views.close_activity, name='close_category'),
     path('api/admin/create-project', views.create_project, name='create_project'),
     path('api/admin/update-project/<int:id>', views.update_project, name='update_project'),
@@ -14,7 +12,6 @@ urlpatterns = [
     path('api/vote-project', views.vote_project, name='vote_poject'),
     path('api/vote-expositor', views.vote_expositor, name='vote_expositor'),
     path('api/get-members', views.get_members, name='get_members'),
-    path('api/get-categorys', views.get_categorys, name='get_categorys'),
     path('api/get-category/<int:id>', views.get_category, name='get_category'),
     path('api/get-projects', views.get_projects, name='get_projects'),
     path('api/get-project/<int:id>', views.get_project, name='get_project'),
@@ -29,7 +26,6 @@ urlpatterns = [
     path('api/get-activities', views.get_activities, name='get_activities'),
     path('api/get-activity/<int:id>', views.get_activity, name='get_activity'),
     path('api/update-activity/<int:id>', views.update_activity, name='update_activity'),
-    path('api/cretae-member', views.create_member, name='create_member'),
     path('api/count-projct-by-category', views.count_project_in_category, name='count_project_by_category'),
     path('api/register-voter', views.register_voter, name='register_voter')
 ]
